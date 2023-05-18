3b27168920c5da4baff8816c9afb2749f2a3e408
thiyangt <thiyanga.talagala@monash.edu>
2023-05-18 20:17:42 +0530

    updating codes

---
+++ data-raw/DATASET.R
@@ -0,0 +1,30 @@
+## code to prepare `DATASET` dataset goes here
+
+usethis::use_data(DATASET, overwrite = TRUE)
+
+# elephants data simulation
+
+AF <- 120
+AM <- 150
+BF <- 70
+BM <- 60
+CF <- 30
+CM <- 40
+
+#  total
+AF + AM + BF + BM + CF + CM #470
+
+# An amazing but nevertheless accurate
+#fact is that an elephant’s height at the shoulder
+#is twice the circumference of his foot.
+
+# ----
+# https://seaworld.org/animals/all-about/elephants/characteristics/
+
+# African-Female
+
+# African-Male
+
+# Asian - Female
+
+# Asian-Male
